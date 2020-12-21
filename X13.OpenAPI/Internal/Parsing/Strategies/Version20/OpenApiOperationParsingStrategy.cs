90d403703e548862630d26641a8dd498475dbba2
Ranul Pallemulle <ranulp96@gmail.com>
2020-12-21 12:56:45 +0530

    Initial Commit

---
+++ X13.OpenAPI/Internal/Parsing/Strategies/Version20/OpenApiOperationParsingStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace X13.OpenAPI.Internal.Parsing.Strategies.Version20
+{
+    class OpenApiOperationParsingStrategy
+    {
+    }
+}
---
+++ X13.OpenAPI/Public/Model/OpenApiXml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace X13.OpenAPI.Public.Model
+{
+    class OpenApiXml
+    {
+    }
+}
