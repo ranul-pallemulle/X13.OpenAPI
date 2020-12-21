750fe4a6db5cb57a5bb677609b81de51bd4a1090
Ranul Pallemulle <ranulp96@gmail.com>
2020-12-21 20:53:39 +0530

    Tests update

--- X13.OpenAPI.Tests/Parsing/OpenApi3/SimpleTests.cs
+++ X13.OpenAPI.Tests/Parsing/OpenApi3/SimpleTests.cs
@@ -10,7 +10,7 @@ namespace X13.OpenAPI.Tests.Parsing.OpenApi3
     public class SimpleTests
     {
         [Fact]
-        public void SimpleDocumentParsingTest()
+        public void SimpleDocumentParsingTest_NoReferences()
         {
             var json = @"
             {
@@ -76,6 +76,84 @@ namespace X13.OpenAPI.Tests.Parsing.OpenApi3
             Assert.NotNull(document.Paths);
             Assert.Equal("Simple OpenAPI Document", document.Info.Title);
             Assert.Equal("v1", document.Info.Version);
+        }
+
+        [Fact]
+        public void SimpleDocumentParsingTest_WithReferences()
+        {
+            var json = @"
+            {
+                ""openapi"": ""3.0.2"",
+                ""info"": {
+                    ""title"": ""Simple OpenAPI Document"",
+                    ""version"": ""v1""
+                },
+                ""servers"": [
+                    {
+                        ""url"": ""https://simple.com""
+                    }
+                ],
+                ""paths"": {
+                    ""/path1"": {
+                        ""get"": {
+                            ""summary"": ""Simple GET Operation"",
+                            ""operationId"": ""simpleGet"",
+                            ""parameters"": [
+                                {
+                                    ""in"": ""query"",
+                                    ""name"": ""param1"",
+                                    ""required"": true,
+                                    ""schema"": {
+                                        ""type"": ""string""
+                                    }
+                                }
+                            ],
+                            ""requestBody"": {
+                                ""$ref"": ""#/components/requestBodies/Body1""
+                            },
+                            ""responses"": {
+                                ""200"": {
+                                    ""description"": ""Ok""
+                                }
+                            }
+                        }
+                    }
+                },
+                ""components"": {
+                    ""requestBodies"": {
+                        ""Body1"": {
+                            ""required"": true,
+                            ""content"": {
+                                ""application/json"": {
+                                    ""schema"": {
+                                        ""type"": ""object"",
+                                        ""properties"": {
+                                            ""name"": {
+                                                ""type"": ""string"",
+                                            },
+                                            ""age"": {
+                                                ""type"": ""integer"",
+                                                ""format"": ""int32""
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            ";
+
+            IOpenApiReader reader = new OpenApiReader();
+            var document = reader.Read(json);
+
+            Assert.NotNull(document.Paths["/path1"]);
+            Assert.NotNull(document.Paths["/path1"].Operations[OpenApiOperationType.Get].RequestBody);
+            var body = document.Paths["/path1"].Operations[OpenApiOperationType.Get].RequestBody;
+            Assert.NotNull(body.Content["application/json"]);
+            Assert.NotNull(body.Content["application/json"].Schema);;
+            Assert.Equal("object", body.Content["application/json"].Schema.Type);
         }
     }
 }
